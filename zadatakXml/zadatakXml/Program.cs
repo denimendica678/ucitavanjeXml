b38ca129ac92c2a5e8f06866bb2e01f64070a895
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-03-08 09:20:50 +0100

    Add files via upload

---
+++ zadatakXml/zadatakXml/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace zadatakXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+          
+
+            XmlDocument doc = new XmlDocument();
+            //Load the the document with the last book node.  
+            XmlTextReader reader = new XmlTextReader(@"C:\Users\Učenik 3\Desktop\vjezba.xml");
+            reader.Read();
+            // load reader   
+            doc.Load(reader);
+            // Display contents on the console  
+            doc.Save(Console.Out);
+
+
+            Console.ReadKey();
+        }
+        
+    }
+}
